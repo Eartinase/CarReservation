89919dc54a31dfdc51f9d0883b508052aac0532b
Eartinase <cutiemoon08@yahoo.com>
2017-03-23 19:48:34 +0700

    edit path .___.

--- application/views/AddCarView.php
+++ application/views/AddCarView.php
@@ -5,15 +5,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
 
-	<script src='../fullcalendar/lib/jquery.min.js'></script>
-	<link rel="stylesheet" type="text/css" href="../fullcalendar/bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="../fullcalendar/bootstrap/css/bootstrap-theme.css">
-	<script src='../fullcalendar/bootstrap/js/bootstrap.js'></script>
+	<script src='fullcalendar/lib/jquery.min.js'></script>
+	<link rel="stylesheet" type="text/css" href="fullcalendar/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="fullcalendar/bootstrap/css/bootstrap-theme.css">
+	<script src='fullcalendar/bootstrap/js/bootstrap.js'></script>
 
 	
 </head>
 <body style="margin:2%">
-	<form action="addCar" method="post">
+	<form action="addCar/addCar" method="post">
 		ทะเบียน: <input type="text" name="plateLicense" class="form-control" required> <br>
 		ประเภทรถ: <select name ="carType" class="form-control" required>
 					<?php 
@@ -24,8 +24,8 @@
 		จำนวนที่นั่ง: <input type="number" class="form-control" name="seat" required> <br>
 		คนขับ: <select name = "driver" class="form-control" required>
 					<?php 
-					//echo $driver ;
-					echo var_dump($driver);
+					echo $driver ;
+					//echo var_dump($driver);
 					?>
 				</select>
 		<br>
