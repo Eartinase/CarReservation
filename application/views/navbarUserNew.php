7b4d9e5c6746ffc9e2576d31d95f2cba491e6bb3
Nisa <deviruchi@gmail.com>
2017-05-03 04:01:44 +0700

    จัดหน้า

--- application/views/navbarUserNew.php
+++ application/views/navbarUserNew.php
@@ -1,44 +1,45 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top" >
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
+    <div class="navbar-header" >
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a style="margin-left:22px" float class="navbar-brand" href="#">
+      <a  float class="navbar-brand" href="#">
         <img src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/home-128.png" width="28" height="28"></a>
-        <a style="font-size:25px" class="navbar-brand" href="#">เมนูหลัก</a>
+        <a  class="navbar-brand" href="#">เมนูหลัก</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a style="font-size:16px" href="#" data-toggle="dropdown">ใช้บริการรถภายนอก <span class="sr-only">(current)</span></a></li>
-        <li><a style="font-size:16px"href="#" data-toggle="dropdown">ดูประวัติการใช้บริการรถ</a></li>
-        <li class="dropdown">
-          <a style="font-size:16px" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">สร้างรายงาน<span class="caret"></span></a>
+        <li style="padding-left: 0px;"><a href="#" data-toggle="dropdown">ขอใช้รถภายนอก <span class="sr-only">(current)</span></a></li>
+        <li style="padding-left: 0px;"><a  href="#" data-toggle="dropdown">ประวัติการใช้รถ</a></li>
+        <li class="dropdown" style="padding-left: 0px;">
+          <a  href="#" class="dropdown-toggle" data-toggle="dropdown">สร้างรายงาน<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <!--<li><a href="#">Something else here</a></li>-->
-            <li><a href="#">รายงานประวัติการใช้บริการรถ</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">รายงานขอเบิกงบประมาณ</a></li>
+            <li style="padding-left: 0px;"><a href="#">รายงานประวัติการใช้บริการรถ</a></li>
+            <li style="padding-left: 0px;" role="separator" class="divider"></li>
+            <li style="padding-left: 0px;"><a href="#">รายงานขอเบิกงบประมาณ</a></li>
           </ul>
         </li>
       </ul>
     
       <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a style="font-size:18px" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ชื่อ-นามสกุลผู้ใช้งาน<span class="caret"></span></a>
+        <li style="padding-left: 0px;" class="dropdown">
+          <a  href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ชื่อ-นามสกุลผู้ใช้งาน<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">แก้ไขข้อมูลส่วนตัว</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">ออกจากระบบ</a></li>
+            <li style="padding-left: 0px;"><a href="#">แก้ไขข้อมูลส่วนตัว</a></li>
+            <li style="padding-left: 0px;" role="separator" class="divider"></li>
+            <li style="padding-left: 0px;"><a href="#">ออกจากระบบ</a></li>
           </ul>
         </li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+
+
