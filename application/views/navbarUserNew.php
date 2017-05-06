3f7ad4dcc16e4d5deef677e660a1826cce094f5f
Nisa <deviruchi@gmail.com>
2017-05-06 20:27:12 +0700

    แก้ style li

--- application/views/navbarUserNew.php
+++ application/views/navbarUserNew.php
@@ -16,26 +16,26 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a style="font-size:16px" href="#" data-toggle="dropdown">ใช้บริการรถภายนอก <span class="sr-only">(current)</span></a></li>
-        <li><a style="font-size:16px"href="#" data-toggle="dropdown">ดูประวัติการใช้บริการรถ</a></li>
-        <li class="dropdown">
+        <li style="padding-left:0px;"><a style="font-size:16px" href="#" data-toggle="dropdown">ใช้บริการรถภายนอก <span class="sr-only">(current)</span></a></li>
+        <li style="padding-left:0px;" ><a style="font-size:16px"href="#" data-toggle="dropdown">ดูประวัติการใช้บริการรถ</a></li>
+        <li style="padding-left:0px;" class="dropdown">
           <a style="font-size:16px" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">สร้างรายงาน<span class="caret"></span></a>
           <ul class="dropdown-menu">
             <!--<li><a href="#">Something else here</a></li>-->
-            <li><a href="#">รายงานประวัติการใช้บริการรถ</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">รายงานขอเบิกงบประมาณ</a></li>
+            <li style="padding-left:0px;"><a href="#">รายงานประวัติการใช้บริการรถ</a></li>
+            <li style="padding-left:0px;" role="separator" class="divider"></li>
+            <li style="padding-left:0px;"><a href="#">รายงานขอเบิกงบประมาณ</a></li>
           </ul>
         </li>
       </ul>
     
       <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
+        <li style="padding-left:0px;" class="dropdown">
           <a style="font-size:18px" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ชื่อ-นามสกุลผู้ใช้งาน<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">แก้ไขข้อมูลส่วนตัว</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="<?php echo base_url(); ?>/Login">ออกจากระบบ</a></li>
+            <li style="padding-left:0px;"><a href="#">แก้ไขข้อมูลส่วนตัว</a></li>
+            <li style="padding-left:0px;" role="separator" class="divider"></li>
+            <li style="padding-left:0px;"><a href="<?php echo base_url(); ?>/Login">ออกจากระบบ</a></li>
           </ul>
         </li>
       </ul>
