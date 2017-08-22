b7335ae2492702a8b5e7bbd865d5cbba8c289865
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-08-23 01:00:52 +0700

    ประวัติจองรถตามคัน V.2

--- application/views/ReserveHistoryByCar.php
+++ application/views/ReserveHistoryByCar.php
@@ -12,6 +12,180 @@ ob_start(); // ทำการเก็บค่า html นะครับ
 	<div class="con">
 		<h2 style="text-align:center">รายงานการจองรถมหาวิทยาลัย</h2>
 		<h3 style="text-align:center">ตั้งแต่วันที่ _____ ถึง _____</h3>
+
+
+		<table>	
+			<tr>
+				<th colspan="5" style="text-align:center">4กย 7904</th>
+			</tr>
+			<tr>
+				<td style="text-align:center">เวลาไป</td>
+				<td style="text-align:center">เวลากลับ</td>
+				<td style="text-align:center">สถานที่</td>
+				<td style="text-align:center">หน่วยงstyle="text-align:center"าน</td>
+				<td style="text-align:center">เบอร์ติดต่อ</td>
+			</tr>
+			<tr>
+				<td>20/08/2017 09:00:00</td>
+				<td>20/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>SIT</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>23/08/2017 09:00:00</td>
+				<td>23/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>MTE</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>24/08/2017 12:00:00</td>
+				<td>24/08/2017 13:00:00</td>
+				<td>NASA</td>
+				<td>ENE</td>
+				<td>083-3337333</td>
+			</tr>
+		</table>
+<br>
+		<table>	
+			<tr>
+				<th colspan="5" style="text-align:center">4กย 7904</th>
+			</tr>
+			<tr>
+				<td style="text-align:center">เวลาไป</td>
+				<td style="text-align:center">เวลากลับ</td>
+				<td style="text-align:center">สถานที่</td>
+				<td style="text-align:center">หน่วยงstyle="text-align:center"าน</td>
+				<td style="text-align:center">เบอร์ติดต่อ</td>
+			</tr>
+			<tr>
+				<td>20/08/2017 09:00:00</td>
+				<td>20/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>SIT</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>23/08/2017 09:00:00</td>
+				<td>23/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>MTE</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>24/08/2017 12:00:00</td>
+				<td>24/08/2017 13:00:00</td>
+				<td>NASA</td>
+				<td>ENE</td>
+				<td>083-3337333</td>
+			</tr>
+		</table>
+
+		<br>
+		<table>	
+			<tr>
+				<th colspan="5" style="text-align:center">4กย 7904</th>
+			</tr>
+			<tr>
+				<td style="text-align:center">เวลาไป</td>
+				<td style="text-align:center">เวลากลับ</td>
+				<td style="text-align:center">สถานที่</td>
+				<td style="text-align:center">หน่วยงstyle="text-align:center"าน</td>
+				<td style="text-align:center">เบอร์ติดต่อ</td>
+			</tr>
+			<tr>
+				<td>20/08/2017 09:00:00</td>
+				<td>20/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>SIT</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>23/08/2017 09:00:00</td>
+				<td>23/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>MTE</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>24/08/2017 12:00:00</td>
+				<td>24/08/2017 13:00:00</td>
+				<td>NASA</td>
+				<td>ENE</td>
+				<td>083-3337333</td>
+			</tr>
+		</table>
+
+		<br>
+		<table>	
+			<tr>
+				<th colspan="5" style="text-align:center">4กย 7904</th>
+			</tr>
+			<tr>
+				<td style="text-align:center">เวลาไป</td>
+				<td style="text-align:center">เวลากลับ</td>
+				<td style="text-align:center">สถานที่</td>
+				<td style="text-align:center">หน่วยงstyle="text-align:center"าน</td>
+				<td style="text-align:center">เบอร์ติดต่อ</td>
+			</tr>
+			<tr>
+				<td>20/08/2017 09:00:00</td>
+				<td>20/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>SIT</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>23/08/2017 09:00:00</td>
+				<td>23/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>MTE</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>24/08/2017 12:00:00</td>
+				<td>24/08/2017 13:00:00</td>
+				<td>NASA</td>
+				<td>ENE</td>
+				<td>083-3337333</td>
+			</tr>
+		</table>
+
+		<br>
+		<table>	
+			<tr>
+				<th colspan="5" style="text-align:center">4กย 7904</th>
+			</tr>
+			<tr>
+				<td style="text-align:center">เวลาไป</td>
+				<td style="text-align:center">เวลากลับ</td>
+				<td style="text-align:center">สถานที่</td>
+				<td style="text-align:center">หน่วยงstyle="text-align:center"าน</td>
+				<td style="text-align:center">เบอร์ติดต่อ</td>
+			</tr>
+			<tr>
+				<td>20/08/2017 09:00:00</td>
+				<td>20/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>SIT</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>23/08/2017 09:00:00</td>
+				<td>23/08/2017 17:00:00</td>
+				<td>KX</td>
+				<td>MTE</td>
+				<td>083-3333333</td>
+			</tr>
+			<tr>
+				<td>24/08/2017 12:00:00</td>
+				<td>24/08/2017 13:00:00</td>
+				<td>NASA</td>
+				<td>ENE</td>
+				<td>083-3337333</td>
+			</tr>
+		</table>	
 		<p></p>
 		
 	</body>
