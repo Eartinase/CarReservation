d3934e6aaa7709c9bbcc4c3eb2af50b76b957773
DESKTOP-P5CS000\Tickymill <ticky.joker.v@gmail.com>
2017-08-25 20:06:12 +0700

    เจน pdf แบบดึงข้อมูลจริงมาแสดงจาก db

--- application/controllers/GenReport.php
+++ application/controllers/GenReport.php
@@ -5,12 +5,30 @@ class genReport extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		//$this->load->model('CarsModel','CarsModel');		
+		$this->load->model('ReservationModel','ReservationModel');
+		$this->load->model('CarsModel','CarsModel');	
+		$this->load->model('UserModel','UserModel');		
 	}
 
-	public function genReserveHistoryByUser(){				
-		$this->load->view('ReserveHistoryByUser');
+	public function genPDFUserHistory(){	
+		//$empCode = $this->session->userdata['logged_in']['employeeCode'];
+		$reserveInfo = $this-> ReservationModel->getCurReserveFormEmpCode(0);
+		$userInfo = $this-> UserModel->getUserInfo('admin');
+		$carType = array();
+		foreach ($reserveInfo as $value) {
+			$car =$this->CarsModel->getCarById($value->getCarId());
+			$id = $car->getCarId();
+			$v = $car->getCarType();
+			$carType[$id] = $v;
+		}
+
+
+		$data['reserveInfo'] = $reserveInfo;
+		$data['userInfo']= $userInfo;
+		$data['carType'] = $carType;
+		$this->load->view('GenPDFUserHistory',$data);
 	}
+
 
 	public function genReserveHistoryByCar(){				
 		$this->load->view('ReserveHistoryByCar');
---
+++ application/views/GenPDFUserHistory.php
@@ -0,0 +1,45 @@
+<?php
+require_once('mpdf/mpdf.php'); //ที่อยู่ของไฟล์ mpdf.php ในเครื่องเรานะครับ
+ob_start(); // ทำการเก็บค่า html นะครับ
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<div class="con">
+<h2 style="text-align:center;font-size:40px">รายงานประวัติการใช้บริการรถ</h2>
+<p style="text-align:center;font-size:20px">ชื่อ-นามสกุลผู้จอง <?php echo $userInfo->getName() ?> หน่วยงาน <?php echo $userInfo->getDepartment() ?> ตำแหน่ง <?php echo $userInfo->getRole() ?></p>
+
+<table style="font-size:20px;border: 1px solid #ddd;text-align: center;border-collapse: collapse;width: 100%" >
+  <tr>
+    <th style="padding: 12px;border: 1px solid #ddd">หมายเลขการจอง</th>
+    <th style="padding: 12px;border: 1px solid #ddd">ประเภทรถ</th>
+    <th style="padding: 12px;border: 1px solid #ddd">ทะเบียนรถ</th>
+    <th style="padding: 12px;border: 1px solid #ddd">วันเวลาที่เดินทาง</th>
+    <th style="padding: 12px;border: 1px solid #ddd">วันเวลาที่กลับ</th>
+    <th style="padding: 12px;border: 1px solid #ddd">สถานที่</th>
+  </tr>
+  <?php foreach ($reserveInfo as $value) { ?>
+  <tr>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $value->getReserveId() ?></td>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $carType[$value->getCarId()] ?></td>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $value->getPlateLicese() ?></td>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $value->getStartDate() ?></td>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $value->getEndDate() ?></td>
+    <td style="padding: 12px;border: 1px solid #ddd"><?php echo $value->getPlace() ?></td>
+  </tr>
+  <?php } ?>
+</table>
+
+</body>
+</html>	
+<?php
+$html = ob_get_contents();
+ob_end_clean();
+$pdf = new mPDF('tha', 'A4', '0', 'thsarabun'); //การตั้งค่ากระดาษถ้าต้องการแนวตั้ง ก็ A4 เฉยๆครับ ถ้าต้องการแนวนอนเท่ากับ A4-L
+//$pdf->SetAutoFont();
+
+$pdf->SetDisplayMode('fullpage');
+$pdf->WriteHTML($html, 2);
+$pdf->Output('รายงานประวัติการใช้บริการรถ.pdf', 'I');
+?>
