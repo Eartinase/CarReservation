8c403e12a531361108b88a8bf3f5249d10ea38fb
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-08-30 22:12:10 +0700

    Modal for driver activated :D

--- application/controllers/Driver.php
+++ application/controllers/Driver.php
@@ -12,21 +12,8 @@ class Driver extends CI_Controller {
 	public function index(){		
 		$data = $this->ReservationModel->driverReserve();
 		$table = "";
-		foreach ($data->result() as $row){
-			$table .= "<tr>";
-			$table .= "<td>".$row->currentid."</td>";
-			$table .= "<td>".$row->CarType."</td>";
-			$table .= "<td>".$row->PlateLicense."</td>";
-			$table .= "<td>".$row->Name."</td>";
-			$table .= "<td>".$row->place."</td>";
-			$table .= "<td>".$row->StartDate."</td>";
-			$table .= "<td>".$row->EndDate."</td>";
-			$table .= "<td>active</td>";
-			$table .= "<td><button class='btn btn-success' name='".$row->currentid."''>ยืนยัน</button></td>";
-		}	
-
 		$send = array(
-			'tab' => $table
+			'tab' => $data
 		);
 
 		$this->load->view('DriverView', $send);
