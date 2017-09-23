6ef74b0233dbed88ad49a9d95d953ce8d11076b0
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-09-24 02:27:07 +0700

    edit and delete car // still buggy

--- application/controllers/ManageCar.php
+++ application/controllers/ManageCar.php
@@ -2,17 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ManageCar extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('UserModel');
+		$this->load->model('CarsModel');
+	}
 
 	public function index(){
 		$this->load->view('AllCar');
 	}
 
-	public function AddCar(){
-		$this->load->model('UserModel');
-		
+	public function AddCar(){		
 		$driverOption = $this->UserModel->getDriver();
 		$depOption = $this->UserModel->getDepartment();
-				
+
 		$data = array(
 			'driverOption' => $driverOption,
 			'depOption' => $depOption
@@ -22,9 +25,8 @@ class ManageCar extends CI_Controller {
 
 	public function ajax_carlist(){
 		$draw = intval($this->input->get("draw"));
-        $start = intval($this->input->get("start"));
-		//$depID = $this->session->userdata['logged_in']['department'];
-		$this->load->model('CarsModel');
+		$start = intval($this->input->get("start"));
+		//$depID = $this->session->userdata['logged_in']['department'];	
 		$allCar = $this->CarsModel->getAllCar();
 		$data = array();
 		$no=0;
@@ -33,25 +35,88 @@ class ManageCar extends CI_Controller {
 				//$car = $this->CarsModel->getCarById($value->getCarId());
 				$no++;
 				$data[] = array(					
-	            	"<center>".$value->getPlateLicese()."</center>",
-	               	"<center>".$value->getCarType()."</center>",	   	         
-	                "<center>".$value->getBrand()." ".$value->getGeneration()."</center>",
-	                "<center>".$value->getSerial()."</center>",
-	                "<center>".$value->getSeat()."</center>",
-	                "<center>".$value->getItemLabel()."</center>",
-	                "<center>".$value->getFuel()."</center>",
-	                "<center>".$value->getDepartment()."</center>",
-	                "<center>".$value->getDescription()."</center>",
-	               	"<p>...</p>"
-	            );			
+					"<center>".$value->getPlateLicese()."</center>",
+					"<center>".$value->getCarType()."</center>",	   	         
+					"<center>".$value->getBrand()."\n".$value->getGeneration()."</center>",
+					"<center>".$value->getSerial()."</center>",
+					"<center>".$value->getSeat()."</center>",
+					"<center>".$value->getItemLabel()."</center>",
+					"<center>".$value->getFuel()."</center>",
+					"<center>".$value->getDepartment()."</center>",
+					"<center>".$value->getDescription()."</center>",
+
+					"<form method='post' action='".base_url()."ManageCar/EditCar'>\n".
+					"<input type='text' hidden name='carId' value='".$value->getCarId()."'>\n".
+					"<button class='btn btn-success' type='submit'>รายละเอียด</button>\n".
+					"</form>"	               		               	
+				);			
 			}
 		}
-		 $output = array(            
-                "data" => $data
-            );		
+		$output = array(            
+			"data" => $data
+		);		
 		//output to json format
 		echo json_encode($output);
 		exit;
+	}
+
+	public function EditCar(){
+		$driverOption = $this->UserModel->getDriver();
+		$depOption = $this->UserModel->getDepartment();
+
+		$carDetail = $this->CarsModel->getCarForEdit($_POST["carId"]);		
+		
+		$data = array(
+			'color' 		=> $carDetail['color'],
+			'plateLicense' 	=> $carDetail['plateLicense'],
+			'carType' 		=> $carDetail['carType'],
+			'registerDate'	=> $carDetail['registerDate'],
+			'price' 		=> $carDetail['price'],
+			'brand' 		=> $carDetail['brand'],
+			'generation' 	=> $carDetail['generation'],
+			'serial' 		=> $carDetail['serial'],
+			'purchaseYear' 	=> $carDetail['purchaseYear'],
+			'seat' 			=> $carDetail['seat'],
+			'itemLabel' 	=> $carDetail['itemLabel'],
+			'driver' 		=> $carDetail['driver'],
+			'department' 	=> $carDetail['department'],
+			'description' 	=> $carDetail['description'],
+			'generation' 	=> $carDetail['generation'],
+			'fuel' 			=> $carDetail['fuel'],
+			'carId' 		=> $carDetail['carId'],
+			'depId' 		=> $carDetail['depId'],
+			'depOption'		=> $depOption,
+			'driverOption'	=> $driverOption
+		);
+		$this->load->view('EditCar',$data);
+	}
+
+	public function EditOrDel(){
+		$carId = $_POST["carId"];
+
+		if (isset($_POST['confirm'])) {
+			$data = array(		
+				'PlateLicense'	=> $this->input->post('plateLicense'), 
+				'Color' 		=> $this->input->post('color'),
+				'CarTypeId' 	=> $this->input->post('carType'),
+				'RegisterDate'	=> $this->input->post('RegisterDate'),
+				'Price'			=> $this->input->post('price'),
+				'Brand'			=> $this->input->post('brand'),
+				'Generation'	=> $this->input->post('generation'),
+				'serial'		=> $this->input->post('serial'),
+				'PurchaseYear'	=> $this->input->post('PurchaseYear'),
+				'Seat' 			=> $this->input->post('seat'),
+				'itemLabel'		=> $this->input->post('itemLabel'),
+				'DriverId' 		=> $this->input->post('driverId'),
+				'fuel'			=> $this->input->post('Fuel'),
+				'depID'			=> $this->input->post('depId'),
+				'description'	=> $this->input->post('description')	
+			);		
+			$this -> CarsModel -> Update($data, $carId);
+		}elseif (isset($_POST['del'])) {
+			$this->CarsModel->deleteCar($carId);
+		}
+		$this->load->view('AllCar');
 	}
 
 }
--- application/views/EditCar.php
+++ application/views/EditCar.php
@@ -12,15 +12,17 @@
 	?>
 	<div class="container">
 		<center>
-			<h3>เพิ่มรถ</h3>
+			<h3>แก้ไขรถ</h3>
 		</center>
-		<form action="<?php echo base_url(); ?>AddCar/Add" method="post">
+		<form action="<?php echo base_url(); ?>ManageCar/EditOrDel" method="post">
 			<div class="row">
 				<div class="col-md-6">
-					ทะเบียน: <input type="text" name="plateLicense" class="form-control" required> 
+					ทะเบียน: 
+					<input type="text" name="plateLicense"  class="form-control" value="<?php echo $plateLicense ?>" required> 
 				</div>
 				<div class="col-md-6">
-					สี: <input type="text" name="color" class="form-control" required>
+					สี: 
+					<input type="text" name="color" class="form-control" value="<?php echo $color ?>" required>
 				</div>
 			</div>
 			<br>
@@ -35,53 +37,53 @@
 						<option value="4">ไมโครบัส</option>
 					</select>
 				</div>				
-				<div class="col-md-6">
+				<div class="col-md-6" >
 					วันที่จดทะเบียน:
-					<input type="date" name="RegisterDate" class="form-control">
+					<input type="date" name="RegisterDate" class="form-control" value="<?php echo $registerDate ?>">
 				</div>
 			</div>
 			<br>
 			<div class="row">
 				<div class="col-md-6">
 					ราคา:
-					<input type="number" name="price" class="form-control" required>
+					<input type="number" name="price" class="form-control"  value="<?php echo $price ?>" required>
 				</div>				
 				<div class="col-md-6">
 					ยี่ห้อ:
-					<input type="text" name="brand" class="form-control" required> 
+					<input type="text" name="brand" class="form-control"  value="<?php echo $brand ?>" required> 
 				</div>
 			</div>
 			<br>
 			<div class="row">
 				<div class="col-md-6">
 					แบบ/รุ่น: 
-					<input type="text" name="generation" class="form-control" required>
+					<input type="text" name="generation" class="form-control" value="<?php echo $generation ?>" required>
 				</div>				
 				<div class="col-md-6">
 					หมายเลขเครื่อง: 
-					<input type="text" name="serial" class="form-control" required>
+					<input type="text" name="serial" class="form-control" value="<?php echo $serial ?>" required>
 				</div>
 			</div>
 			<br>
 			<div class="row">
 				<div class="col-md-6">
 					ปีที่ซื้อ:
-					<input type="number" name="PurchaseYear" class="form-control" required>
+					<input type="number" name="PurchaseYear" class="form-control" value="<?php echo $purchaseYear ?>" required>
 				</div>				
 				<div class="col-md-6">
 					ขนาด (จำนวนที่นั่ง): 
-					<input type="text" name="seat" class="form-control" required>
+					<input type="text" name="seat" class="form-control" value="<?php echo $seat ?>" required>
 				</div>
 			</div>
 			<br>
 			<div class="row">
 				<div class="col-md-6">
 					หมายเลขครุภัณฑ์: 
-					<input type="text" name="itemLabel" class="form-control">
+					<input type="text" name="itemLabel" class="form-control" value="<?php echo $itemLabel ?>">
 				</div>				
 				<div class="col-md-6">
 					พนักงานขับรถ: 
-					<select class="form-control" name="driverId" id="driverOption">
+					<select class="form-control" name="driverId" id="driverOption" required>
 						<option> - - </option>
 						<?php 						
 						echo $driverOption;
@@ -93,12 +95,12 @@
 			<div class="row">
 				<div class="col-md-6">
 					เชื้อเพลิง: 
-					<input type="text" name="Fuel" class="form-control" required>
+					<input type="text" name="Fuel" class="form-control" value="<?php echo $fuel ?>">
 				</div>
 				<div class="col-md-6">
 					หน่วยงานเจ้าของ:
-					<select class="form-control" name="depId">
-						<option > - - </option>
+					<select value="<?php echo $depId  ?>" class="form-control" name="depId" >
+						<option selected disabled hidden> - - </option>
 						<?php 						
 						echo $depOption;
 						?>
@@ -109,16 +111,28 @@
 			<div class="row">
 				<div class="col-md-12">
 					หมายเหตุ:
-					<textarea class="form-control" name="description"></textarea>
+					<textarea class="form-control" name="description"><?php echo $description ?></textarea>
 				</div>				
 			</div>
 			<br>
 			
 			<center>
-				<button type="submit" class="btn btn-primary" >ยืนยัน</button>
+				<input type="text" name="carId" hidden value="<?php echo $carId?>">
+
+				<button type="submit" class="btn btn-primary" name="confirm">ยืนยัน</button>
+				<button class="btn btn-danger" onclick="confirmDel()" name="del">ลบรถ</button>
 			</center>
+
 		</form>
 		<br>
 	</div>
+	<script type="text/javascript">
+		function confirmDel() {
+			if(confirm("คุณต้องการลบรถคันนี้หรือไม่?")){
+
+			}
+		}
+		
+	</script>
 </body>
 </html>
